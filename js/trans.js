4d2ba58ec8c62b57cf76e5522c6c2b3bb57cf5c0
cbHasib <hasibul.pbn@gmail.com>
2022-08-17 18:18:09 +0600

    Organize code

--- js/trans.js
+++ js/trans.js
@@ -1,10 +1,9 @@
 let count = 0;
 
-
 function transictionHistory(method, amount) {
-    count++;
+  count++;
 
-    // Date 
+  // Date
   let today = new Date();
   const dd = String(today.getDate()).padStart(2, "0");
   const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
@@ -12,27 +11,21 @@ function transictionHistory(method, amount) {
   today = dd + "/" + mm + "/" + yyyy;
   // Date
 
-
-
-
   const tableBody = document.getElementById("table-body");
-
   const tableRow = document.createElement("tr");
-  
-  tableRow.innerHTML = `
-                      <td class="border border-slate-400 p-2 text-center font-semibold">${count}</td>
+
+  let classAmount = "text-black";
+
+  if (method === "Deposit") {
+    tableRow.classList.add("bg-green-100");
+    classAmount = "text-green-800";
+  } else {
+    tableRow.classList.add("bg-red-100");
+    classAmount = "text-red-800";
+  }
+  tableRow.innerHTML = `<td class="border border-slate-400 p-2 text-center font-semibold">${count}</td>
                       <td class="border border-slate-400 p-2">${method}</td>
                       <td class="border border-slate-400 p-2 text-center">${today}</td>
-                      <td class="border border-slate-400 p-2 text-center font-medium text-green-800">$<span>${amount}</span></td>
-  `;
-  
-  if(method === 'Deposit'){
-      tableRow.classList.add("bg-green-100");
-  }
-  else{
-      tableRow.classList.add("bg-red-100");
-  }
-  console.log(method.toLowerCase);
-  
+                      <td class="border border-slate-400 p-2 text-center font-medium ${classAmount}">$<span>${amount}</span></td>`;
   tableBody.appendChild(tableRow);
 }
